69cffef59a8abeba61cbcded74ffba0ba3c48dab
Denis Ivanov <visible@jumph4x.net>
2014-06-19 12:29:11 -0700

    GH initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Calidriftbears::Application.config.secret_key_base = '495a4a80c336d1a01cd0f2e0ad058cf18ba1f02e8247b33c4332923ad810f3677a328b2d3c6d7422c71cb5ee45dfb7652bb5a596109350131949ea3e33e4011f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Calidriftbears::Application.config.session_store :cookie_store, key: '_calidriftbears_session'
